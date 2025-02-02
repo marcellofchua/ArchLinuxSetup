81f26114821c49dae2fa7befb2ef2433c09f84cc
Mar <marcello.f.chua@gmail.com>
2025-02-02 11:38:07 -1000

    Update InstallMenu.sh

--- InstallMenu.sh
+++ InstallMenu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 PS3="Select item please: "
-items=("Minimal Setup No GUI" "Install Hyprland" "Main Software" "Other Software" "dotFiles" "Change to ZSH")
+items=("Minimal Setup No GUI" "Install Hyprland" "Main Software" "Other Software" "dotFiles" "Configure ZSH")
 while true; do
     select item in "${items[@]}" Quit
     do
@@ -14,24 +14,23 @@ while true; do
             3) echo "Selected item #$REPLY which means $item"
 		yay -S fastfetch wofi mako pavucontrol kate okular thunar-volman swww python-pywal16 sox\
 		python jdk-openjdk jdk21-openjdk jdk17-openjdk\
-		steam vesktop mullvad-vpn code-oss\
+		steam vesktop mullvad-vpn code\
   		grim slurp swappy feh cava icat imgcat imgmagick pqiv ueberzugpp ffmpeg ffmpegthumbnailer viu tumbler\
 		noto-fonts noto-fonts-cjk ttf-jetbrains-mono ttf-firacode ttf-nerd-font-symbols;
 		break;;
             4) echo "Selected item #$REPLY which means $item"
 		yay -S ghostty docker electron gnome-boxes qbittorrent qemu virt-manager\
-		lua nasm ncurses perl neovim\
-		tty-clock cavalier khal wttr yt-dlp ytfzf traceroute npm octave neovim gifski\
-		anki blender vlc obsidian proton wf-recorder sddm paleta lxappearance qt5-styleplugins\
+		lua nasm ncurses perl neovim anki obsidian wf-recorder python-pywalfox16\
+		tty-clock cavalier khal wttr yt-dlp ytfzf traceroute npm octave gifski\
+		blender vlc proton sddm paleta lxappearance qt5-styleplugins qutebrowser\
 		xdg-desktop-portal-gtk kdeconnect gimp ungoogled-chromium duolingo-desktop-bin;
 		curl -fsSL "https://raw.githubusercontent.com/SteamClientHomebrew/Millennium/main/scripts/install.sh" | sh;
 		break;;
 	    5) echo "Selected item #$REPLY which means $item"
 		cp -r Pictures/* ~/Pictures;
 		swww img ~/Pictures/Wallpapers/toyama.gif;
-		cp -r dotFiles/dotConfig ~/.config;
-		cp -r dotFiles/dotUser/.bash_profile ~/;
-		cp -r dotFiles/dotUser/.zshrc ~/;
+		cp -r dotFiles/dotConfig/* ~/.config;
+		cp -r dotFiles/dotUser/* ~/;
 		cp -r dotFiles/dotMozilla/chrome ~/.mozilla/firefox/*.default-release/;
 		mkdir ~ .themes;
 		mkdir ~ .icons;
@@ -51,6 +50,7 @@ while true; do
        		yay -S --noconfirm zsh-theme-powerlevel10k-git;
 		echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc;
 		chsh -s $(which zsh);
+  		git clone https://github.com/zsh-users/zsh-syntax-highlighting /usr/share/
 		break;;
             $((${#items[@]}+1))) echo "Quit InstallerMenu.sh"; break 2;;
             *) echo "Oopsie Woopsie. $REPLY"; break;
