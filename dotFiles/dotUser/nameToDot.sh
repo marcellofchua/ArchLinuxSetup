d9c81cda3ea955ced1a77c6c1cee68f6d9275b49
Mar <marcello.f.chua@gmail.com>
2025-02-02 16:34:29 -1000

    Update nameToDot.sh

--- dotFiles/dotUser/nameToDot.sh
+++ dotFiles/dotUser/nameToDot.sh
@@ -1,4 +1,4 @@
-mv bashrc .bashrc
-mv gtkrc-2.0 .gtkrc-2.0
-mv p10k.zsh .p10k.zsh
-mv zshrc .zshrc
+mv ~/bashrc ~/.bashrc
+mv ~/gtkrc-2.0 ~/.gtkrc-2.0
+mv ~/p10k.zsh ~/.p10k.zsh
+mv ~/zshrc ~/.zshrc
