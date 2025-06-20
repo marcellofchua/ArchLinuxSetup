797ed163a1118bb9e282deba03e35213007649b5
Mar <marcello.f.chua@gmail.com>
2025-06-20 10:09:40 -1000

    Delete Pictures directory

--- Pictures/AnImageResizer.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-read -p "Input filename: " input
-read -p "Output filename: " output
-INPUT="$input"
-OUTPUT="$output"
-ffmpeg -hide_banner -v warning -i $INPUT -filter_complex "[0:v] scale=640:-1:flags=lanczos,split [a][b]; [a] palettegen=reserve_transparent=on:transparency_color=ffffff [p]; [b][p] paletteuse" $OUTPUT
