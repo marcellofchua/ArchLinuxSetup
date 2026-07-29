24a4234088cab9e3c885141a14e063ae4ed3147d
Mar <marcello.f.chua@gmail.com>
2026-07-28 17:58:58 -0700

    Update .tmux-audio.sh

--- dotUser/.tmux-audio.sh
+++ dotUser/.tmux-audio.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 
+SESSION_NAME="Audio"
+
 ### Step 0: Check if tmux session already exists
+if tmux has-session -t $SESSION_NAME 2>/dev/null; then
+	echo "Session $SESSION_NAME already exists."
+	tmux attach-session -t $SESSION_NAME
+else
 
 ### Step 1: Create tmux sessions if it doesn't exist yet
-tmux new-session -d -s Audio
+	tmux new-session -d -s Audio
 
 ### Configure Pulsemixer window
-tmux split-window -v
-tmux resize-pane -U 25
-tmux send-keys -t 0 pulsemixer ENTER
+	tmux split-window -v
+	tmux resize-pane -U 25
+	tmux send-keys -t 0 pulsemixer ENTER
 
 ### Configure yt-x window
-tmux split-window -h
-tmux send-keys -t 1 yt-x ENTER
+	tmux split-window -h
+	tmux send-keys -t 1 yt-x ENTER
 
 ### Configure cava window
-tmux send-keys -t 2 cava ENTER
+	tmux send-keys -t 2 cava ENTER
 
 ### Step 2: Attach to tmux sessions
-tmux attach
+	tmux attach-session -t $SESSION_NAME
+fi
